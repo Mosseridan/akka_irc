68d09b8d0357f5ffe9747743215b92ebd65dcd3a
Liad Ginosar <liadginosar@gmail.com>
2017-05-25 15:45:19 +0300

    basic functionality

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+import akka.actor.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    /*
+    ActorSystem system = ActorSystem.create("Hello");
+    ActorRef a = system.actorOf(Props.create(HelloWorld.class), "helloWorld");
+    system.actorOf(Props.create(Terminator.class, a), "terminator");
+    */
+    ActorSystem system = ActorSystem.create("IRC");
+    ActorRef a = system.actorOf(Props.create(ServerActor.class), "Server");
+    system.actorOf(Props.create(Terminator.class, a), "terminator");
+
+  }
+
+  public static class Terminator extends AbstractLoggingActor {
+
+    private final ActorRef ref;
+
+    public Terminator(ActorRef ref) {
+      this.ref = ref;
+      getContext().watch(ref);
+    }
+
+    @Override
+    public Receive createReceive() {
+      return receiveBuilder()
+              .match(Terminated.class, t -> {
+                log().info("{} has terminated, shutting down system", ref.path());
+                getContext().system().terminate();
+              })
+              .build();
+    }
+  }
+}
